79c86adc9139b131873ebdd54cd502a9f17bb8c4
Paula TPC <pautpc@MacBook-Air-de-Pau.local>
2023-10-17 17:27:49 -0500

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Paula2208.github.io.com',
+    site: 'https://paula2208.github.io.com',
     //base: '/pau-portafolio',
 });
