f0f39bfb425f153f41fa907157b55f6ea2bd1d55
Paula TPC <pautpc@MacBook-Air-de-Pau.local>
2023-10-17 13:36:29 -0500

    First deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://paucode.com',
+    //base: '/pau-portafolio',
+});
